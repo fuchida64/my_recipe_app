025aacca8e6965be8230a83307e8be2a1cb12cb3
Yutaro Fuchida <yutarof64@gmail.com>
2020-03-20 01:47:38 +0900

    #64 add category show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,11 @@
 class CategoriesController < ApplicationController
+
   def index
     @categories = Category.all
+  end
+
+  def show
+    @category = Category.find_by(name: params[:id])
+    @recipes = @category.recipes if @category
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :ingredients, only: [:index, :create]
   resources :recipes, only: [:index, :new, :create, :show, :edit, :update]
-  resources :categories, only: [:index]
+  resources :categories, only: [:index, :show]
   resources :menus, only: [:index, :create]
 
   get '/search', to: 'recipes#search', as: 'search'
