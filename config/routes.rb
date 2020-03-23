d091e2faeb9adbdc7b267f201c80cf567abcb139
Yutaro Fuchida <yutarof64@gmail.com>
2020-03-24 04:32:53 +0900

    #69 add menu update/destroy

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :ingredients, only: [:index, :create]
   resources :recipes, only: [:index, :new, :create, :show, :edit, :update]
   resources :categories, only: [:index, :show]
-  resources :menus, only: [:index, :create]
+  resources :menus, only: [:index, :create, :update, :destroy]
 
   get '/search', to: 'recipes#search', as: 'search'
 end
